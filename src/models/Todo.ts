12a51273cf61d3ac11d5628c17d682c9d7140b47
adiandoyo32 <akhiang32@gmail.com>
2022-04-23 18:10:13 +0700

    feat: todo model interface

---
+++ src/models/Todo.ts
@@ -0,0 +1,7 @@
+export default interface Todo {
+    id: number;
+    title: string;
+    description: string;
+    status: 0;
+    createdAt: string;
+}
---
+++ src/models/response/TodoResponse.ts
@@ -0,0 +1,19 @@
+import Todo from "../Todo";
+
+export default interface TodoResponse {
+    id: number;
+    title: string;
+    description: string;
+    status: 0;
+    createdAt: string;
+}
+
+export const mapTodoResponseToResponse = (response: TodoResponse): Todo => {
+    return {
+        id: response.id,
+        title: response.title,
+        description: response.description,
+        status: response.status,
+        createdAt: response.createdAt,
+    };
+};
