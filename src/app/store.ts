436b97dd654edd3362181f43c2ca40a47d1087dd
adiandoyo32 <akhiang32@gmail.com>
2022-04-24 00:46:05 +0700

    fix: remove unused counter module

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import todoReducer from '../redux/todo-slice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     todo: todoReducer,
   },
 });
