2fb8a92996fffb2e91e85676e03a431719405ca8
adiandoyo32 <akhiang32@gmail.com>
2022-04-23 23:48:51 +0700

    fix: update todo card ui

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,18 +1,34 @@
 import React from "react";
+import { useAppDispatch } from "../app/hooks";
 import Todo from "../models/Todo";
+import { FormState, setFormState, setTodo } from "../redux/todo-slice";
+import TodoStatusBadge from "./TodoStatusBadge";
 
 interface TodoCardProps {
     todo: Todo;
+    onClick: () => void;
 }
 
 const TodoCard: React.FC<TodoCardProps> = (props) => {
+    const dispatch = useAppDispatch();
+
     return (
-        <div className="card w-96 bg-base-100 shadow-xl my-4">
+        <div className="card w-100 bg-base-100 shadow-xl my-4">
             <div className="card-body">
                 <h2 className="card-title">{props.todo.title}</h2>
-                <p>{props.todo.description}</p>
+                <TodoStatusBadge status={props.todo.status} />
+                <p className="card-text">{props.todo.description}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary"></button>
+                    <button
+                        className="btn btn-primary btn-xs"
+                        onClick={() => {
+                            dispatch(setTodo(props.todo));
+                            dispatch(setFormState(FormState.VIEW))
+                            props.onClick();
+                        }}
+                    >
+                        Detail
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,30 +1,75 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { selectTodo, setTodoList } from "../redux/todo-slice";
+import { FormState, resetTodo, selectTodo, setCompletedTodo, setFormState, setTodoList } from "../redux/todo-slice";
 import { loadTodoList } from "../services/todo-service";
+import PlusIcon from "./PlusIcon";
 import TodoCard from "./TodoCard";
+import TodoModal, { useModalDialog } from "./TodoModal";
 
 const TodoList = () => {
     const todoState = useAppSelector(selectTodo);
     const dispatch = useAppDispatch();
+    const { visible, toggle } = useModalDialog();
 
     const init = async () => {
         const todoList = await loadTodoList();
         dispatch(setTodoList(todoList));
     };
 
+    const completedTodoList = () => {
+        const completed = todoState.todoList.filter(todo => todo.status == 1);
+        console.log(completed)
+    }
+
     useEffect(() => {
         init();
         return () => {};
     }, []);
 
+    useEffect(() => {
+        completedTodoList()
+    }, [todoState.todoList]);
+
     return (
-        <>
-            <div className="text-3xl font-bold underline">TodoList</div>
-            {todoState.todoList.map((todo, index) => (
-                <TodoCard key={index} todo={todo} />
-            ))}
-        </>
+        <div className="p-4">
+            <div className="flex mt-12 space-x-4 justify-center">
+                <div className="text-3xl font-bold">Todo App</div>
+                <button
+                    type="button"
+                    className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-blue-600 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={() => {
+                        dispatch(resetTodo());
+                        dispatch(setFormState(FormState.CREATE));
+                        toggle();
+                    }}
+                >
+                    <PlusIcon />
+                    Create
+                </button>
+            </div>
+
+            <div className="flex mb-20">
+                <div className="w-1/2 h-12 p-4">
+                    <div className="text-2xl font-semibold">Uncompleted Todo</div>
+                    {todoState.todoList.map((todo, index) =>
+                        todo.status == 0 ? <TodoCard key={index} todo={todo} onClick={toggle} /> : null
+                    )}
+                </div>
+                <div className="w-1/2 h-12 p-4">
+                    <div className="text-2xl font-semibold">Completed Todo</div>
+                    {todoState.todoList.map((todo, index) =>
+                        todo.status == 1 ? <TodoCard key={index} todo={todo} onClick={toggle} /> : null
+                    )}
+                </div>
+                {
+                    todoState.completedTodoList.map((todo) => {
+                        return  <div>{todo.title}</div>
+                    })
+                }
+            </div>
+
+            <TodoModal visible={visible} toggle={toggle} />
+        </div>
     );
 };
 
