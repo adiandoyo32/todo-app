1ed0222bd50425dbd5780dd53c99deb2f9acae86
adiandoyo32 <akhiang32@gmail.com>
2022-04-23 23:52:16 +0700

    feat: todo redux set, add, edit, delete todo

--- src/redux/todo-slice.ts
+++ src/redux/todo-slice.ts
@@ -1,14 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../app/store";
+import { TodoFormField } from "../components/TodoFormViewModel";
 import Todo from "../models/Todo";
 
+export enum FormState {
+    CREATE = "ADD",
+    EDIT = "EDIT",
+    VIEW = "VIEW",
+}
+
 export interface TodoState {
+    formState: FormState.CREATE | FormState.EDIT | FormState.VIEW;
     todoList: Todo[];
+    completedTodoList: Todo[];
     todo: Todo;
 }
 
 const initialState: TodoState = {
+    formState: FormState.CREATE,
     todoList: [],
+    completedTodoList: [],
     todo: {
         id: 0,
         title: "",
@@ -18,17 +29,63 @@ const initialState: TodoState = {
     },
 };
 
+const findTodoIndex = (todoList: Todo[], id: number): number => {
+    return todoList.findIndex((todo) => todo.id == id);
+};
+
+const generateCreateAt = () => {
+    const date = new Date();
+    var day = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    var time = date.getHours() + ":" + date.getMinutes();
+    return day + " " + time;
+};
+
 export const todoSlice = createSlice({
     name: "todo",
     initialState,
     reducers: {
+        resetTodo: (state) => {
+            state.todo = {
+                ...initialState.todo,
+            };
+        },
+        setCompletedTodo: (state, { payload }: PayloadAction<Todo[]>) => {
+            // const completed = payload.map((todo) => {
+            //     if (todo.status == 1) return todo;
+            // });
+            // const temp = completed.sort((a, b) => a!.createdAt!.getTime() - b!.createdAt!.getTime());
+        },
+        setFormState: (state, action: PayloadAction<FormState>) => {
+            state.formState = action.payload;
+        },
         setTodoList: (state, { payload }: PayloadAction<Todo[]>) => {
             state.todoList = payload;
+        },
+        setTodo: (state, { payload }: PayloadAction<Todo>) => {
+            state.todo = payload;
+        },
+        addTodo: (state, { payload }: PayloadAction<TodoFormField>) => {
+            const newTodo: Todo = {
+                ...payload,
+                id: Date.now(),
+                createdAt: generateCreateAt(),
+                // createdAt: new Date(),
+            };
+            state.todoList.push(newTodo);
+        },
+        editTodo: (state, { payload }: PayloadAction<Todo>) => {
+            const index = findTodoIndex(state.todoList, payload.id);
+            state.todoList[index] = payload;
+        },
+        deleteTodo: (state, { payload }: PayloadAction<number>) => {
+            const index = findTodoIndex(state.todoList, payload);
+            state.todoList.splice(index, 1);
         },
     },
 });
 
-export const { setTodoList } = todoSlice.actions;
+export const { setTodoList, setCompletedTodo, setTodo, addTodo, editTodo, deleteTodo, resetTodo, setFormState } =
+    todoSlice.actions;
 export const selectTodo = (state: RootState) => state.todo;
 
 export default todoSlice.reducer;
